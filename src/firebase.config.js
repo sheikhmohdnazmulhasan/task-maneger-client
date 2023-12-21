1f6d1a6b422f3bc52f8d7553ae32f732c1c8e674
mdnazmul42726 <nazmulofficial@outlook.com>
2023-12-21 20:00:57 +0600

    initialized project

---
+++ src/firebase.config.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDrfAt5joQ8X-zemk5ShFDg27b-UUQkX50",
+    authDomain: "ph-b8-assignment-9.firebaseapp.com",
+    projectId: "ph-b8-assignment-9",
+    storageBucket: "ph-b8-assignment-9.appspot.com",
+    messagingSenderId: "318214884559",
+    appId: "1:318214884559:web:5265e75deb8195a9eefbba"
+};
+
+const app = initializeApp(firebaseConfig);
