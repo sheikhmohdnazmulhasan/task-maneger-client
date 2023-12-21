f1dbc36dd3891432541316ddca6d0d90fdedc646
mdnazmul42726 <nazmulofficial@outlook.com>
2023-12-21 22:28:42 +0600

    impliment live chat

--- src/Root.jsx
+++ src/Root.jsx
@@ -36,11 +36,11 @@ const Root = () => {
                     {/* Page content here */}
                     <Outlet />
                 </div>
-                <div className="drawer-side">
+                <div className="drawer-side ">
                     <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 min-h-full bg-base-200">
+                    <ul className="menu p-4 w-80 min-h-full bg-base-200 z-50">
                         {/* Sidebar content here */}
-                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar  1</a></li>
                         <li><a>Sidebar Item 2</a></li>
                     </ul>
                 </div>
