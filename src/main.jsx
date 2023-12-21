4eb0f7789cc6e48f73d3e738d96e9e49da3cbbd0
mdnazmul42726 <nazmulofficial@outlook.com>
2023-12-21 20:09:44 +0600

    navabr added

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Root from "./Root";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <div>Hello world!</div>,
+    path: "/", element: <Root />,
   },
 ]);
 
