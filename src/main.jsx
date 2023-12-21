0e92ecb9a62e69b41b89beea2c9e5162b91e0f14
mdnazmul42726 <nazmulofficial@outlook.com>
2023-12-22 00:35:53 +0600

    auth provider created

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Root from "./Root";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import AuthProvider from "./AuthProvider";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider> <RouterProvider router={router} /></AuthProvider>
   </React.StrictMode>
 );
